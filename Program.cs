253f30f6a4e96e21562ecee3b76efda59fea4e64
Евгений <evgeniykotin@gmail.com>
2021-02-27 20:58:01 +0200

    Форматирование кода

--- Program.cs
+++ Program.cs
@@ -18,55 +18,55 @@ namespace LT_ShortVersion
                 Console.WriteLine("Для запуска программы нажми '+'. Для выхода из приложения нажми '-'");
 
                 string choice = Console.ReadLine();
-                
-                    switch (choice)
-                    {
-                        case "+":
+
+                switch (choice)
+                {
+                    case "+":
                         {
                             string ticket;
                             string check_ticket;
 
                             while (true)
-                                {
-                                    Console.WriteLine($"Введите номер вашего билета: ");
-                                    ticket = Console.ReadLine();
-                                    check_ticket = new String(ticket.Where(x => Char.IsDigit(x)).Take(8).ToArray());
+                            {
+                                Console.WriteLine($"Введите номер вашего билета: ");
+                                ticket = Console.ReadLine();
+                                check_ticket = new String(ticket.Where(x => Char.IsDigit(x)).Take(8).ToArray());
 
                                 if (check_ticket.Length < 3)
                                 {
                                     Console.WriteLine("Введите номер вашего билета корректно! \n");
                                 }
-                                else { break; };  
-                                }
-
-                                if (check_ticket.Length % 2 != 0)
-                                {
-                                    check_ticket = check_ticket.Insert(0, "0");
-                                }
-
-                                int[] arr = check_ticket.Select(x => int.Parse(x.ToString())).ToArray();
-
-                                for (int i = 0; i < arr.Length; i++)
-                                {
-                                    if (i < (arr.Length + 1) / 2)
-                                    {
-                                        sum_1 += arr[i];
-                                    }
-                                    else sum_2 += arr[i];
-                                }
-                                if (sum_1 != sum_2)
-                                {
-                                    Console.WriteLine($"Ваш билет #{check_ticket} - не счастливый :( ");
-                                }
-                                else
-                                {
-                                    Console.WriteLine($"Ваш билет #{check_ticket} - счатливый :)");
-                                }
-                                break;
+                                else { break; };
                             }
-                        case "-": { start = false; Console.WriteLine("Программа завершенна"); break; }
-                    }
+
+                            if (check_ticket.Length % 2 != 0)
+                            {
+                                check_ticket = check_ticket.Insert(0, "0");
+                            }
+
+                            int[] arr = check_ticket.Select(x => int.Parse(x.ToString())).ToArray();
+
+                            for (int i = 0; i < arr.Length; i++)
+                            {
+                                if (i < (arr.Length + 1) / 2)
+                                {
+                                    sum_1 += arr[i];
+                                }
+                                else sum_2 += arr[i];
+                            }
+                            if (sum_1 != sum_2)
+                            {
+                                Console.WriteLine($"Ваш билет #{check_ticket} - не счастливый :( ");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Ваш билет #{check_ticket} - счатливый :)");
+                            }
+                            break;
+                        }
+                    case "-": { start = false; Console.WriteLine("Программа завершенна"); break; }
                 }
+            }
             Console.Read();
         }
     }
